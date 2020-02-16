bab097087544ce53e46aa37f3f07b2cbb0dfb003
Lucas Gallo <lucasagallo@gmail.com>
2020-02-16 13:14:45 -0300

    working a little

--- get_next_line.c
+++ get_next_line.c
@@ -11,7 +11,11 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
+
+/*
 void	ft_get_numbers(char *buf)
 {
 	int		size;
@@ -71,12 +75,48 @@ int		ft_read_file(char *name)
 	ft_get_numbers(buf);
 	return (1);
 }
+*/
 int		get_next_line(int fd, char **line)
 {
 	char	*buf;
-	int		ret;
+	static int	ret;
+	int	i;
 
 	if (fd == -1)
 		return (-1);
-	
+	i = -1;
+	buf = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	while ((ret = read(fd, &buf[++i], 1)))
+	{
+	//	printf("[buf(%d)=%c]", i, buf[i]);		
+		if (buf[i] == '\n')
+		{
+			buf[i + 1] = '\0';
+			//printf("%s", buf);
+			*line = buf;
+			free(buf);
+			return (1);
+		}
+	}
+	return (0);
 }
+
+int		main(int argc, char **argv)
+{
+	int	fd;
+	char	*str;
+
+	(void)argv;
+	if (argc == 1)
+		fd = open("teste.txt", O_RDONLY);
+	else
+		fd = 0;
+	if (fd == -1)
+		return (-1);
+	while (get_next_line(fd, &str))
+		printf("%s", str);
+	if (close(fd) == -1)
+		return (-1);
+	return (0);
+}
+
