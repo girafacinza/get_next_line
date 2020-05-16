288a60963b1f20ec95d2d6873ad2cf7091f9f02c
Lucas Gallo <lucasagallo@gmail.com>
2020-05-16 20:18:41 -0300

    after all changes. need to test

--- get_next_line.h
+++ get_next_line.h
@@ -3,25 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lambrozi <lambrozi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/12 19:49:43 by lambrozi          #+#    #+#             */
-/*   Updated: 2020/03/10 21:40:08 by lambrozi         ###   ########.fr       */
+/*   Created: 2020/02/18 17:21:06 by maraujo-          #+#    #+#             */
+/*   Updated: 2020/05/10 19:50:30 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H 
+# define GET_NEXT_LINE_H
 
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
+# include <limits.h>
 
 int		get_next_line(int fd, char **line);
-char	*ft_strdup(char *s1, int i);
-char	*ft_substr(char const *s, int start, int len);
-int 	ft_strlen(const char *s);
-char	*ft_strjoin(char *str1, char *str2);
+int		ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(const char *s1);
 
 #endif
